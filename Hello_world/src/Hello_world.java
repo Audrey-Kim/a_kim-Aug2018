6748a225242a1682c405d5e076e344cfa5728852
Audrey Kim <32147698+Audrey-Kim@users.noreply.github.com>
2018-08-20 09:45:58 -0700

    First project to commit to Github! For 2018

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,15 @@
+/**
+ * The class Hello_world *CONTINUE DESCRIPTION*
+ * 
+ * @author Audrey
+ * @version 0 (8/17/18)
+ */
+public class Hello_world {
+	
+	public static void main(String[] args) {
+		
+		//Displays string 
+		System.out.println("Hello World! From Audrey");
+	}
+
+}
